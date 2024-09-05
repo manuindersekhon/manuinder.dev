775b9b6255f52f7dc9fa39a90b25917bb29da891
Manuinder Sekhon <sekhon.manuinder@gmail.com>
2024-09-05 15:09:06 +0530

    move favicons to public folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,10 @@ export default function RootLayout({ children }: Readonly<{ children: any }>) {
     <html lang='en'>
       <head>
         <ColorSchemeScript defaultColorScheme='light' />
-        <link rel='shortcut icon' href='/images/favicon.ico' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/images/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/images/favicon-16x16.png' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
         <meta charSet='utf-8' name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
       </head>
       <body>
